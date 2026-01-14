cb709d3274beb78cad8e6303125271dd32b59c2d
Abdul Rahman <76593253+jhabdulrahman@users.noreply.github.com>
2026-01-14 15:20:47 +0530

    Update MongoDB connection string password

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,2 +1,2 @@
-const connectionString = 'mongodb+srv://jhabdulrahman_db_user:HMadmin25@nodeproject.g62pnns.mongodb.net/?appName=NodeProject';
+const connectionString = 'mongodb+srv://jhabdulrahman_db_user:test@nodeproject.g62pnns.mongodb.net/?appName=NodeProject';
 const mongoose = require('mongoose');
